93c49f8bee097f76e19ed6634d9afa0dd6ba7317
Mamay Christian <mamay.mboma@hotmail.com>
2021-06-03 14:40:21 +0200

    first commit

---
+++ src/test/java/be/iepscf/uccle/rentalcarapi/RentalCarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iepscf.uccle.rentalcarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
